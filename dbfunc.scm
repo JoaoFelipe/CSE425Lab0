46594d572b5a86060e73d3926641a0e08920f180
Joao Felipe Nicolaci Pimentel <jfn1@shell.cec.wustl.edu>
2012-09-13 16:38:56 -0500

    lookup function working

--- dbfunc.scm
+++ dbfunc.scm
@@ -1,8 +1,37 @@
 ; dbfunc.scm
 
+; This function searchs for a data filename (dat) in a opened port of a repository file (rep-port)
+; The repository file should be sorted
+(define find-in-file (lambda (dat rep-port)  
+		(let ((obj (read rep-port))) 
+			(cond 
+				((eof-object? obj) 
+					(begin
+						(close-input-port rep-port)
+						#f
+					)
+				) 
+				(else 
+					(cond
+						((string=? dat (symbol->string obj)) #t)
+						((string<? dat (symbol->string obj)) #f)
+						(else
+							(find-in-file dat rep-port)
+						)
+					)
+				)
+			)
+		)
+))
+
+; This function lookup for a data filename (dat) in a repository file (rep)
+; The params should be the name of the files
 (define fn-lookup (lambda (dat rep)
-	(display dat) (newline)
-	(display rep) (newline)
+	(if (find-in-file dat (open-input-file rep))
+		(display (string-append "The data file '" dat "' is listed on the repository file '" rep "'")) 
+		(display (string-append "The data file '" dat "' is NOT listed on the repository file '" rep "'"))
+	)
+	(newline)
 ))
 
 (define fn-print (lambda (dat rep)
